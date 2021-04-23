cb2439e3d1023ee448f743a22486990221dec4b5
RichardFi <627838766@qq.com>
2021-04-23 17:30:17 +1000

    Update search panel

--- src/screens/projectList/searchPanel.jsx
+++ src/screens/projectList/searchPanel.jsx
@@ -1,7 +1,28 @@
-export const SearchPanel = () =>{
+import {useState} from "react";
+
+export const SearchPanel = () => {
     const [param, setParam] = useState({
         name: '',
         userId: ''
     })
-    return <form action=""></form>
+    const [users,setUsers] = useState([]);
+
+    //setParam(Object.assign({}, param, ))
+    return <form action="">
+        <div>
+            <input type="text" value={param.name} onChange={evt => setParam({
+                ...param,
+                name: evt.target.value
+            })} />
+            <select value={param.personId} onChange={evt => setParam({
+                ...param,
+                userId: evt.target.value
+            })}>
+                <option value={''}>Person</option>
+                {
+                    users.map(user => <option value={user.id}>{user.name}</option>)
+                }
+            </select>
+        </div>
+    </form>
 }
