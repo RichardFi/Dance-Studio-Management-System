57ae65d3953699ad088997eb68231861e2b8297d
RichardFi <627838766@qq.com>
2021-05-02 17:03:10 +1000

    Modified login and register styled component

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,25 +1,50 @@
 import React, { useState } from "react"
 import { RegisterScreen } from "unauthenticated-app/register";
 import { LoginScreen } from "unauthenticated-app/login";
-import { Card } from 'antd';
+import { Card, Divider } from 'antd';
 import styled from '@emotion/styled';
 
 export const UnauthenticatedApp = () => {
     const [isRegister, setIsRegister] = useState(false);
-    return <div style={{ display: 'flex', justifyContent: "center" }}>
-        <Card>
+    return <Container>
+        <Header />
+        <ShadowCard>
+            <Title>
+                {isRegister ? 'Register to Zero One':'Login to Zero One'}
+            </Title>
             {
                 isRegister ? <RegisterScreen /> : <LoginScreen />
             }
-            <button onClick={() => setIsRegister(!isRegister)}>change to {isRegister ? 'login' : 'register'}</button>
-        </Card>
-
-    </div>
+            <Divider />
+            <a onClick={() => setIsRegister(!isRegister)}>
+                {isRegister ? 'Sign in with a email and password' : `Don't have an account?`}
+            </a>
+        </ShadowCard>
+    </Container>
 }
+const Title = styled.h2`
+margin-bottom: 2.4rem;
+color: rgb(94,108,132);
+`
+
+const Header = styled.header`
+padding: 5rem 0;
+width: 100%;
+`
+
+const ShadowCard = styled(Card)`
+width: 40rem;
+min-height:56rem;
+padding: 3.2rem, 4rem;
+border-radius: 0.3rem;
+box-sizing: border-box;
+box-shadow: rgba(0,0,0,0.1) 0 0 10px;
+text-align: center;
+`
 
 const Container = styled.div`
-display.flex;
-flex-direction:column;
-align-item: center;
+display: flex;
+flex-direction: column;
+align-items: center;
 min-height: 100vh;
 `
